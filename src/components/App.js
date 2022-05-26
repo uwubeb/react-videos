23cbc5d3b4d9bdb784ea0bb2cd4654859cf6f448
Arūnas Zamalis <8914148+uwubeb@users.noreply.github.com>
2022-05-26 21:38:29 +0300

    SearchBar setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <SearchBar />
       </div>
     );
   }
